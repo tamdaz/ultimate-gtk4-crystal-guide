94efa6ce33276c40e69632d527391af79b65d66c
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-06-13 19:36:32 +0200

    Add a new sidebar for the french language.

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -21,12 +21,14 @@ export default {
             {
                 text: 'Language',
                 items: [
-                    { text: 'English', link: '/en/' }
+                    { text: 'English', link: '/en/' },
+                    { text: 'Français', link: '/fr/' }
                 ]
             }
         ],
         sidebar: {
-            '/en/': sidebarEn()
+            '/en/': sidebarEn(),
+            '/fr/': sidebarFr()
         }
     }
 }
@@ -115,6 +117,94 @@ function sidebarEn() {
             collapsible: true,
             items: [
                 { text: 'Flatpak', link: '/en/packaging/flatpak' },
+            ]
+        },
+    ]
+}
+
+function sidebarFr() {
+    return [
+        {
+            text: 'Introduction',
+            items: [
+                { text: 'Bienvenue', link: '/fr/' }
+            ]
+        },
+        {
+            text: 'Installation',
+            collapsible: true,
+            items: [
+                { text: 'Linux et BSD', link: '/fr/installation/linux-and-bsd' },
+                { text: 'Shard', link: '/fr/installation/shard' },
+            ]
+        },
+        {
+            text: 'Structure',
+            collapsible: true,
+            items: [
+                { text: 'Template', link: '/fr/structure/template' },
+                { text: 'Organisation', link: '/fr/structure/organization' },
+            ]
+        },
+        {
+            text: 'Concepts',
+            collapsible: true,
+            items: [
+                { text: 'Widgets', link: '/fr/concepts/widgets' },
+                { text: 'Signals', link: '/fr/concepts/signals' },
+                { text: 'Actions', link: '/fr/concepts/actions' },
+                { text: 'GResource', link: '/fr/concepts/gresource' },
+                { text: 'Débogage', link: '/fr/concepts/debugging' },
+            ]
+        },
+        {
+            text: 'Première application',
+            collapsible: true,
+            items: [
+                { text: 'Plan', link: '/fr/first-app/plan' },
+                { text: 'Design de l\'IU', link: '/fr/first-app/ui-design' },
+                { text: 'Code', link: '/fr/first-app/code' },
+                { text: 'Métadonnées', link: '/fr/first-app/metadata' },
+                { text: 'Traductions', link: '/fr/first-app/translations' },
+                { text: 'GResource', link: '/fr/first-app/gresource' },
+                { text: 'Empaquetage', link: '/fr/first-app/packaging' },
+            ]
+        },
+        {
+            text: 'Guide pour débutants',
+            collapsible: true,
+            items: [
+                { text: 'Introduction', link: '/fr/tutorial/introduction' },
+                { text: 'Bien démarrer', link: '/fr/tutorial/getting_started' },
+                { text: 'Ajouter une vue de contenu', link: '/fr/tutorial/content_view' },
+                { text: 'Charger un contenu à partir d\'un fichier', link: '/fr/tutorial/opening_files' },
+                { text: 'Afficher la position du curseur', link: '/fr/tutorial/cursor_position' },
+                { text: 'Sauvegarder le contenu dans un fichier', link: '/fr/tutorial/saving_files' },
+                { text: 'Sauvegarder l\'état de l\'application', link: '/fr/tutorial/saving_state' },
+                { text: 'Notifier l\'utilisateur avec les toasts', link: '/fr/tutorial/adding_toasts' },
+                { text: 'Forcer le thème le mode sombre', link: '/fr/tutorial/dark_mode' },
+            ]
+        },
+        {
+            text: 'Extra',
+            collapsible: true,
+            items: [
+                { text: 'Parralélisme et concurrence', link: '/fr/extra/parallelism-and-concurrency' },
+                { text: 'Gettext (i18n)', link: '/fr/extra/gettext' },
+                { text: 'Makefile', link: '/fr/extra/makefile' },
+                { text: 'CLI', link: '/fr/extra/cli' },
+                { text: 'Blueprint', link: '/fr/extra/blueprint' },
+                { text: 'Crédits', link: '/fr/extra/credits' },
+                { text: 'Libadwaita', link: '/fr/extra/libadwaita' },
+                { text: 'Liaisons C', link: '/fr/extra/bindings' },
+                { text: 'Rucksack', link: '/fr/extra/rucksack' },
+            ]
+        },
+        {
+            text: 'Empaquetage',
+            collapsible: true,
+            items: [
+                { text: 'Flatpak', link: '/fr/packaging/flatpak' },
             ]
         },
     ]
